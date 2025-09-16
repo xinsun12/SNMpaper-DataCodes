54e2e32c205c9639634b7b7e588896538d3b6705
Daniel McCoy <dmccoy801@gmail.com>
2025-09-16 09:24:26 -0700

    New MATLAB script to generate ROMS figures
    Updated script following reviewer comments

--- Sun_et_al_ROMS_figures.m
+++ Sun_et_al_ROMS_figures.m
@@ -3,8 +3,9 @@
 % One with comammox (obj2)
 
 % Switch needed to process output
-if (0)
-	clearvars -except obj1 obj2
+process = 0;
+if process
+	clearvars -except obj1 obj2 obj3 obj3 obj4 
 	warning off
 	addpath /data/project1/demccoy/romsObj/
 	addpath /data/project1/demccoy/ROMS/peru_chile_0p1/analysis/microbes/paper
@@ -21,23 +22,48 @@ if (0)
 	domain   = [41 401 261 461];
 
 	% Get objects
-	try; obj1;
+	% Reduced run1
+	try; obj1; obj1 = clearROMS(obj1);
 	catch
 		obj1 = initROMS(romsObj,simName,runName1,'domain',domain);
 	end
-	try; obj2;
+	% Reduced run2
+	try; obj2; obj2 = clearROMS(obj2);
 	catch
 		obj2 = initROMS(romsObj,simName,runName2,'domain',domain);
 	end
-else
-	addpath scripts
+	% Full run1
+	try; obj3; obj3 = clearROMS(obj3); 
+	catch
+		obj3 = initROMS(romsObj,simName,runName1);
+	end
+	% Full run2
+	try; obj4; obj4 = clearROMS(obj4); 
+	catch
+		obj4 = initROMS(romsObj,simName,runName2);
+	end
 end
 
 % Get plotdir
+addpath scripts
 pltdir = ['plots/'];
 
+% Commands
+disp([' 1 = Extract profiles from OMZ']);
+disp([' 2 = Transects of O2, NO2, biomass from both simulations']);
+disp([' 3 = Profiles of sources and sinks']); 
+disp([' 4 = Zslice comparisons']);
+disp([' 5 = Biomass scatter and NO2 correlations']);
+disp([' 6 = O2 map snapshot']);
+
+% Choose
+plotchoice = zeros(1,100);
+choice = input('Choose from the above: ');
+plotchoice(choice) = 1;
+
 % Extract profiles from OMZ
-if (0)
+% P1
+if plotchoice(1);
 	% Options
 	vars = {'O2','DOC','DOCR','NO2',...
 	        'AER','NAR','NAI','NAO',...
@@ -135,15 +161,16 @@ if (0)
 end
 
 % Extract and plot transects of O2, NO2, and NO2-->N2 denitrfier biomass from OMZ
-if (1)
+% P2
+if plotchoice(2);
 	% Options
 	vars = {'O2','NO2','NIO'};
 	eta_ind = [120];
 	zgrid = [-805:1:0];
-	files1 = 181:240;
-	files2 = 181:240;
+	files1 = 109:120;
+	files2 = 109:120;
 	dep_lim = [0 600];
-	fontsize = 6;
+	fontsize = 10;
     prof_lon = -83+360;
 
 	% Process or just load data directly
@@ -210,8 +237,8 @@ if (1)
 		'YLim',[dep_lim],...
 		'XLim',[256.7489 282],...
 		'XTick',[260 270 280],...
-		'figtype','sfig',...
-		'figdim',0.33,...
+		'figtype',9.4,...
+		'figdim',0.4,...
 		'cblocation','eastoutside',...
 		'fontsize',fontsize);
         hold on
@@ -221,11 +248,13 @@ if (1)
 	plot(prof_lon,dep_lim(end),'ro','markersize',2,'markerfacecolor','r');
 	ylabel(cb,'$\mu$M','Interpreter','Latex');
 	cb.TickLabelInterpreter = 'Latex';
-	load('data/transect_position_east.mat');
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
 	ax.Position = axpos;
-	cb.Position = cbpos;
 	ax.YDir = 'Reverse';
 	cb.Ticks = [-3 -2 -1 0 1 2 3];
+	set(gca,'YTick',[0 200 400 600]);
 	export_fig('-png',['plots/no2_trans_cmp'],'-m5');
 	close all
 
@@ -242,8 +271,8 @@ if (1)
 		'YLim',[dep_lim],...
 		'XLim',[256.7489 282],...
 		'XTick',[260 270 280],...
-		'figtype','sfig',...
-		'figdim',0.33,...
+		'figtype',9.4,...
+		'figdim',0.4,...
 		'cblocation','eastoutside',...
 		'fontsize',fontsize);
         hold on
@@ -252,10 +281,12 @@ if (1)
 	ylim([dep_lim]);
 	ylabel(cb,'$\mu$M','Interpreter','Latex');
 	cb.TickLabelInterpreter = 'Latex';
-	load('data/transect_position_east.mat');
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
 	ax.Position = axpos;
-	cb.Position = cbpos;
 	ax.YDir = 'Reverse';
+	set(gca,'YTick',[0 200 400 600]);
 	export_fig('-png',['plots/nio_trans_cmp'],'-m5');
 	close all
 
@@ -272,8 +303,8 @@ if (1)
         'YLim',[dep_lim],...
         'XLim',[256.7489 282],...
         'XTick',[260 270 280],...
-        'figtype','sfig',...
-        'figdim',0.33,...
+		'figtype',9.4,...
+		'figdim',0.4,...
         'cblocation','eastoutside',...
         'fontsize',fontsize);
         hold on
@@ -283,17 +314,53 @@ if (1)
 	plot(prof_lon,dep_lim(end),'ro','markersize',2,'markerfacecolor','r');
     ylabel(cb,'$\mu$M','Interpreter','Latex');
     cb.TickLabelInterpreter = 'Latex';
-    load('data/transect_position_east.mat');
-    ax.Position = axpos;
-    cb.Position = cbpos;
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
+	ax.Position = axpos;
     ax.YDir = 'Reverse';
+	set(gca,'YTick',[0 200 400 600]);
     export_fig('-png',['plots/no2_trans_nitrox'],'-m5');
+    close all
+
+    % Get data
+    dat = out2.NO2;
+    lvls = linspace(0,3,21);
+    cmap = cmocean('tempo',20);
+    cmap(1,:) = [1 1 1];
+
+    % Make figure
+    [fig,ax,cb] = plotEtaSlice(tmpdeg,-tmpdep,dat,...
+        'levs',lvls,...
+        'cmap',cmap,...
+        'YLim',[dep_lim],...
+        'XLim',[256.7489 282],...
+        'XTick',[260 270 280],...
+		'figtype',9.4,...
+		'figdim',0.4,...
+        'cblocation','eastoutside',...
+        'fontsize',fontsize);
+        hold on
+    contour(tmpdeg,-tmpdep,out2.O2,[1 1],...
+        'linestyle','--','color',rgb('DimGray'),'linewidth',0.25);
+    ylim([dep_lim]);
+	plot(prof_lon,dep_lim(end),'ro','markersize',2,'markerfacecolor','r');
+    ylabel(cb,'$\mu$M','Interpreter','Latex');
+    cb.TickLabelInterpreter = 'Latex';
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
+	ax.Position = axpos;
+    ax.YDir = 'Reverse';
+	set(gca,'YTick',[0 200 400 600]);
+    export_fig('-png',['plots/no2_trans_comammox'],'-m5');
     close all
 	
 end
 
 % Extract and plot NO2 and production/consumption profiles from OMZ
-if (1)
+% P3
+if plotchoice(3)
 
 	% Options
 	vars = {'O2','NO2','DENITRIF1','DENITRIF2','DENITRIF5','NITROX','NO2AMMOX','ANAMMOX','NO2_UPTAKE','O2_PRODUCTION','O2_CONSUMPTION'};
@@ -549,3 +616,316 @@ if (1)
 	catch
 	end
 end
+
+% Zslice comparisons
+% P4
+if plotchoice(4);
+
+	% Variables
+	vars   = {'O2','NO2'};
+	units  = {'O$_2$ ($\mu$M)','NO$^{-}_2$ ($\mu$M)'};
+	deps   = [120];
+	lvls   = {[0 250],[0 3]};
+	dlvls  = {[-50 50],[-3 3]};
+	cmaps  = {cmocean('-ice',54),cmocean('tempo',54)};
+	dmap   = cmocean('balance',55);
+	files1 = 109:120;
+	files2 = 109:120;
+
+	% Cycle through variables to load results 
+	if (0)
+		for i = 1:length(vars)
+			% Zslice at depth
+			obj3 = zslice(obj3,vars(i),deps,files1);
+			obj4 = zslice(obj4,vars(i),deps,files2);
+
+			% Get averages
+			out1.(vars{i}) = nanmean(squeeze(obj3.data.avg.(vars{i}).slice),3);
+			out2.(vars{i}) = nanmean(squeeze(obj4.data.avg.(vars{i}).slice),3);
+		end
+
+		% Save
+		lon_rho = obj3.grid.lon_rho;
+		lat_rho = obj3.grid.lat_rho;
+        save('data/Sun_et_al_model_data3.mat','out1','out2','lon_rho','lat_rho');
+    else
+        load('data/Sun_et_al_model_data3.mat');
+    end	
+
+	% Cycle through variables to plot
+	for i = 1:length(vars)
+	
+		% Setup contours
+		this_lvls = linspace(lvls{i}(1),lvls{i}(2),55);
+		this_cmap = cmaps{i};
+		this_dlvl = linspace(dlvls{i}(1),dlvls{i}(2),56);
+		this_dmap = dmap;
+
+		% Get anomaly 
+		anom = out1.(vars{i}) - out2.(vars{i});
+
+		% Skip plots?
+		if i == 2 
+		else
+			continue
+		end
+
+		% Make ANOMALY map
+		[fig,cb] = mapPlot_offline(lon_rho,lat_rho,anom,...
+			'levels',this_dlvl,...
+			'cmap',this_dmap,...
+			'figtype','sfig',...
+			'fontsize',10,...
+			'lonticks',[260 270 280],...
+			'latticks',[-20 -10 0],...
+			'latbounds',[-20 0],...
+			'lonbounds',[256.7489 282],...
+			'fontname','Arial');
+		ylabel(cb,units{i},'Interpreter','Latex');
+		hold on
+		if i == 2
+			cb.Ticks = [-3 -2 -1 0 1 2 3];
+			cb.FontName = 'Arial';
+		end
+		m_contour(lon_rho,lat_rho,out1.O2,[1 1],'linestyle','--','color',rgb('DimGray'),'linewidth',0.25);
+		hold on
+		ax = gca;
+		axpos = ax.Position;
+		cbpos = cb.Position;
+		cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
+		ax.Position = axpos;
+		cb.TickLabelInterpreter = 'Latex';
+		export_fig('-png',['plots/zslice_diff_',lower(vars{i})],'-m5');
+		close all
+
+		return
+	end
+
+	clear obj3 obj4
+
+end
+
+% Biomass correlation with NO2 
+% P5
+if plotchoice(5);
+
+    % Get vars
+    vars = {'NIO','NOB','O2','NO2'};
+	files1  = 229:240;
+
+	% Load data
+	if (0)
+		obj1 = loadData(obj1,vars,files1);
+		obj1 = loadDepth(obj1,files1,'full',1);
+
+		% Create 1D arrays
+		for i = 1:length(vars)
+			out.(vars{i}) = obj1.data.avg.(vars{i}).data(:);
+		end
+		out.vol = obj1.grid.avg.volume(:);
+		vars{end+1} = 'vol';
+		obj1 = clearROMS(obj1);
+
+		% Restrict data to low O2 conditions
+		ind = find(out.O2 < 5);
+		for i = 1:length(vars)
+			out.(vars{i}) = out.(vars{i})(ind);
+		end
+
+		% Create meshgrid of biomass
+		bmass_grid = [0:0.005:0.4];
+		[gridX,gridY] = meshgrid(bmass_grid,bmass_grid);
+		gridX = gridX';
+		gridY = gridY';
+		
+		% Get average NO2 concentration within each bin
+		% Get percentages of ROMS values within each bin
+		[a,b] = size(gridX);
+		no2_mag = NaN(a,b);
+		no2_std = NaN(a,b);
+		vol_perc = NaN(a,b);
+		for i = 1:a-1
+			xbini = gridX(i,1);
+			xbinf = gridX(i+1,1);
+			for j = 1:b-1
+				ybini = gridY(1,j);
+				ybinf = gridY(1,j+1);
+				ind = find(...
+					xbini < out.NIO & out.NIO <= xbinf & ...
+					ybini < out.NOB & out.NOB <= ybinf);
+				if ~isempty(ind)
+					tmp = out.NO2(ind).*out.vol(ind); % convert from mmol/m3 to mmol
+					no2_mag(i,j) = sum(tmp)./(sum(out.vol(ind))); % back to mmol/m3
+					no2_std(i,j) = std(out.NO2(ind),out.vol(ind));
+					vol_perc(i,j) = 100*sum(out.vol(ind))/sum(out.vol); 
+				else
+					no2_mag(i,j) = NaN;               
+					no2_std(i,j) = NaN;
+					vol_perc(i,j) = 0;
+				end
+			end
+		end
+
+		% Get discrete NO2 bins
+		no2_bins  = [0:0.05:4];
+		bin_start = no2_bins(1:end-1);
+		bin_end   = no2_bins(2:end);
+		no2_centers = 0.5.*(bin_start + bin_end);
+		for i = 1:length(bin_start)
+			ind = find(bin_start(i)<=out.NO2 & out.NO2<bin_end(i));
+			for j = 1:length(vars)
+				if ~isempty(ind)
+					data(i).(vars{j}) = out.(vars{j})(ind);
+				else
+					data(i).(vars{j}) = NaN;
+				end
+			end
+			tmp = corrcoef(data(i).NIO,data(i).NOB);
+			if isnan(tmp(1,2))
+				correlation(i) = NaN;
+			else
+				correlation(i) = tmp(1,2);
+			end
+		end
+
+        % Save
+        save('data/Sun_et_al_model_data4.mat','gridX','gridY','no2_mag','no2_centers','correlation');
+		return
+    else
+        load('data/Sun_et_al_model_data4.mat');
+    end
+
+	% Meshgrid plots
+	% Magnitude
+	fig = piofigs('mfig',1);
+	ax = gca;
+	set(gca,'FontSize',15,'FontName','Arial');
+	levs = linspace(0,4,101);
+	cmap = cmocean('-thermal',100);
+	cmap(1,:) = [1 1 1];
+	no2_mag(no2_mag>levs(end)) = levs(end);
+	pcolor(gridX,gridY,no2_mag);
+	set(gca,'FontSize',15,'FontName','Arial');
+	shading flat
+	set(gca,'ColorMap',cmap);	
+	cb = colorbar('location','southoutside');
+	axpos = ax.Position;
+	caxis([min(levs) max(levs)]);
+	xlabel('NO$^{-}_2$ Reducer Biomass ($\mu$M C)','Interpreter','Latex');
+	ylabel('NO$^{-}_2$ Oxidizer Biomass ($\mu$M C)','Interpreter','Latex');
+	ylabel(cb,'Volume Weighted NO$^{-}_2$ ($\mu$M N)','Interpreter','Latex','FontSize',15);
+	cb.FontSize = 15;
+	grid off
+	% Shrink
+	ax.Position(1) = ax.Position(1)+0.05;
+	ax.Position(3) = ax.Position(3)-0.05;
+	ax.Position(2) = ax.Position(2)+0.05;
+	ax.Position(4) = ax.Position(4)-0.05;
+	axpos = ax.Position;
+	export_fig('-png',['plots/biomass_mesh_mag'],'-m5');
+    close all
+
+	% Get discrete bins of NO2
+	fig = piofigs('mfig',1);
+	plot(no2_centers,correlation,'-k','linewidth',2);
+	xlabel('Volume Weighted NO$^{-}_2$ ($\mu$M N)','Interpreter','Latex');
+	ylabel('Pearson r coefficient','Interpreter','Latex');
+	set(gca,'FontSize',15,'FontName','Arial');	
+	ax = gca;
+	ax.Position = axpos;
+	grid off
+    export_fig('-png',['plots/biomass_correlation'],'-m5');
+    close all
+
+end
+
+% O2 snapshot map 
+% P6
+if plotchoice(6);
+
+	% Get snapshot from restart
+	if (0)
+		fname  = ['/data/project1/demccoy/ROMS/model_output/peru_chile_0p1/microbes/rst/z_rst_Y0021.00140.nc'];
+
+		% Load O2, reduce to map grid
+		o2_dat  = ncread(fname,'O2');
+		o2_dat  = squeeze(o2_dat(:,:,2));
+		no2_dat = ncread(fname,'NO2');
+		no2_dat = squeeze(no2_dat(:,:,2));
+
+		% Load grid
+		lon_rho = obj3.grid.lon_rho;
+		lat_rho = obj3.grid.lat_rho;
+
+        % Save
+        save('data/Sun_et_al_model_data5.mat','o2_dat','no2_dat','lon_rho','lat_rho');
+		return
+    else
+        load('data/Sun_et_al_model_data5.mat');
+    end
+
+    % Variables
+    units  = {'O$_2$ ($\mu$M)','NO$^{-}_2$ ($\mu$M)'};
+    lvls   = {[0 250],[0 3]};;
+    cmaps  = {cmocean('-ice',54),cmocean('tempo',254)};
+
+	% Make map
+	o2_dat(o2_dat<lvls{1}(1)) = lvls{1}(1);
+	o2_dat(o2_dat>lvls{1}(2)) = lvls{1}(2);
+	this_lvls = linspace(lvls{1}(1),lvls{1}(2),55);
+	this_cmap = cmaps{1}; this_cmap(1,:) = [1 1 1];
+	[fig,cb] = mapPlot_offline(lon_rho,lat_rho,o2_dat,...
+		'levels',this_lvls,...
+		'cmap',this_cmap,...
+		'figtype','sfig',...
+		'fontsize',10,...
+		'lonticks',[260 270 280],...
+		'latticks',[-20 -10 0],...
+		'latbounds',[-20 0],...
+		'lonbounds',[256.7489 282],...
+		'fontname','Times New Roman');
+	ylabel(cb,units{1},'Interpreter','Latex');
+	hold on
+	m_contour(lon_rho,lat_rho,o2_dat,[1 1],'linestyle','--','color',rgb('DimGray'),'linewidth',0.25);
+	ax = gca;
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
+	ax.Position = axpos;
+	cb.TickLabelInterpreter = 'Latex';
+	hold on
+	export_fig('-png',['plots/zslice_o2_snapshot'],'-m5');
+	close all
+
+	% Make map
+	no2_dat(no2_dat<lvls{2}(1)) = lvls{2}(1);
+	no2_dat(no2_dat>lvls{2}(2)) = lvls{2}(2);
+	this_lvls = linspace(lvls{2}(1),lvls{2}(2),255);
+	this_cmap = cmaps{2}; this_cmap(1,:) = [1 1 1];
+	[fig,cb] = mapPlot_offline(lon_rho,lat_rho,no2_dat,...
+		'levels',this_lvls,...
+		'cmap',this_cmap,...
+		'figtype','sfig',...
+		'fontsize',10,...
+		'lonticks',[260 270 280],...
+		'latticks',[-20 -10 0],...
+		'latbounds',[-20 0],...
+		'lonbounds',[256.7489 282],...
+		'fontname','Arial');
+	ylabel(cb,units{1},'Interpreter','Latex');
+	hold on
+	m_contour(lon_rho,lat_rho,o2_dat,[1 1],'linestyle','--','color',rgb('DimGray'),'linewidth',0.25);
+	ax = gca;
+	axpos = ax.Position;
+	cbpos = cb.Position;
+	cb.Position = [cbpos(1) cbpos(2) cb.Position(3)*0.5 cbpos(4)];
+	ax.Position = axpos;
+	cb.TickLabelInterpreter = 'Latex';
+	hold on
+	export_fig('-png',['plots/zslice_no2_snapshot'],'-m5');
+	close all
+	
+end
+
+
+
